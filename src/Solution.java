f3ec5ecd186699dffb50a3e10e39dc9f3dc21bc5
minel1410 <msalihagic@hotmail.com>
2025-03-03 10:34:01 +0100

    Add solution for problem 763

--- src/Solution.java
+++ src/Solution.java
@@ -4,6 +4,7 @@ package src;
 
 
 import java.lang.Math;
+import java.lang.classfile.components.ClassPrinter.ListNode;
 import java.util.*;
 
 import javax.swing.tree.TreeNode;
@@ -2399,5 +2400,41 @@ public class Solution {
         }
 
         return res;
+    }
+
+
+    /*
+     * You are given a string s. We want to partition the string into as many parts
+     * as possible so that each letter appears in at most one part. For example, the
+     * string "ababcc" can be partitioned into ["abab", "cc"], but partitions such
+     * as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+     * 
+     * Note that the partition is done so that after concatenating all the parts in
+     * order, the resultant string should be s.
+     * 
+     * Return a list of integers representing the size of these parts.
+     */
+
+    public List<Integer> partitionLabels(String s) {
+        // Stores the last index of each character in 's'
+        int[] lastOccurrence = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            lastOccurrence[s.charAt(i) - 'a'] = i;
+        }
+
+        int partitionEnd = 0, partitionStart = 0;
+        List<Integer> partitionSizes = new ArrayList<>();
+        for (int i = 0; i < s.length(); ++i) {
+            partitionEnd = Math.max(
+                partitionEnd,
+                lastOccurrence[s.charAt(i) - 'a']
+            );
+            // End of the current partition
+            if (i == partitionEnd) {
+                partitionSizes.add(i - partitionStart + 1);
+                partitionStart = i + 1;
+            }
+        }
+        return partitionSizes;
     }
 }

1ceb5b1bb0d69d03a1788198da76160e5e9ac39c
minel1410 <msalihagic@hotmail.com>
2025-03-19 23:11:44 +0100

    Add solution for problem 2396

--- src/Solution.java
+++ src/Solution.java
@@ -915,5 +915,68 @@ public class Solution {
         
 
     }
+
+
+    /*
+     * An integer n is strictly palindromic if, for every base b between 2 and n - 2
+     * (inclusive), the string representation of the integer n in base b is
+     * palindromic.
+     * 
+     * Given an integer n, return true if n is strictly palindromic and false
+     * otherwise.
+     * 
+     * A string is palindromic if it reads the same forward and backward.
+     */
+
+    /*
+     * In theory this is what the implementation would look like, but since it has been proven
+     * that no number n >= 4 is strictly palindromic we can just simply return false;
+     */
+    public boolean isStrictlyPalindromic(int n) {
+
+
+        //return false;
+
+
+        for(int i = 2; i < n - 1; i++) {
+            String broj = pretvoriBroj(n, i);
+
+            if(!jeLiPalindrom(broj))
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public boolean jeLiPalindrom(String s) {
+
+        int l = 0, r = s.length() - 1;
+
+        while(l <= r) {
+            if(s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+
+        return true;
+
+    }
+
+    public String pretvoriBroj(int n, int baza) {
+        if (n == 0)
+            return "0"; 
+
+        String rez = "";
+        String znakovi = "0123456789ABCDEF"; 
+
+        while (n > 0) {
+            rez = znakovi.charAt(n % baza) + rez;
+            n /= baza;
+        }
+
+        return rez;
+    }
     
 } 

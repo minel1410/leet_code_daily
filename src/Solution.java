e018906c1750d2024174166bc99f766fdde8dbc9
minel1410 <msalihagic@hotmail.com>
2025-03-22 14:28:02 +0100

    Add solution for problem 2243

--- src/Solution.java
+++ src/Solution.java
@@ -1379,4 +1379,46 @@ public class Solution {
             }
         }
     }
+
+
+
+
+    /*
+     * You are given a string s consisting of digits and an integer k.
+     * 
+     * A round can be completed if the length of s is greater than k. In one round,
+     * do the following:
+     * 
+     * Divide s into consecutive groups of size k such that the first k characters
+     * are in the first group, the next k characters are in the second group, and so
+     * on. Note that the size of the last group can be smaller than k.
+     * Replace each group of s with a string representing the sum of all its digits.
+     * For example, "346" is replaced with "13" because 3 + 4 + 6 = 13.
+     * Merge consecutive groups together to form a new string. If the length of the
+     * string is greater than k, repeat from step 1.
+     * Return s after all rounds have been completed.
+     */
+    public String digitSum(String s, int k) {
+
+        while(s.length() > k) {
+            String pom = "";
+            int trenutni = 0;
+            for(int i = 0; i < s.length(); i++) {
+                if((i+1) % k == 0){
+                    trenutni += (int) (s.charAt(i) - '0');
+                    pom += Integer.toString(trenutni);
+                    trenutni = 0;
+                }
+                else
+                    trenutni += (int) (s.charAt(i) - '0');
+            }
+            if(s.length() % k != 0)
+                pom += Integer.toString(trenutni);
+            s = pom;
+        }
+
+        return s;
+
+    }
+
 }

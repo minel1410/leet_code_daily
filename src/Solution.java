af4e5b155c6cff63ac7db14cbf49b30e0b799dfc
minel1410 <msalihagic@hotmail.com>
2025-03-23 23:05:09 +0100

    Add solution for problem 1976

--- src/Solution.java
+++ src/Solution.java
@@ -1543,6 +1543,58 @@ public class Solution {
     }
 
 
+    private static final int MOD = 1_000_000_007;
+
+    public int countPaths(int n, int[][] roads) {
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+
+        for (int[] road : roads) {
+            int u = road[0], v = road[1], time = road[2];
+            graph.get(u).add(new int[] { v, time });
+            graph.get(v).add(new int[] { u, time });
+        }
+
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+        pq.add(new long[] { 0, 0 });
+
+        long[] minTime = new long[n];
+        Arrays.fill(minTime, Long.MAX_VALUE);
+        minTime[0] = 0;
+
+        int[] ways = new int[n];
+        ways[0] = 1;
+
+        while (!pq.isEmpty()) {
+            long[] current = pq.poll();
+            long time = current[0];
+            int node = (int) current[1];
+
+            if (time > minTime[node])
+                continue; 
+
+            for (int[] neighbor : graph.get(node)) {
+                int nextNode = neighbor[0];
+                long newTime = time + neighbor[1];
+
+                if (newTime < minTime[nextNode]) {
+                    minTime[nextNode] = newTime;
+                    pq.add(new long[] { newTime, nextNode });
+                    ways[nextNode] = ways[node]; 
+                } else if (newTime == minTime[nextNode]) {
+                    ways[nextNode] = (ways[nextNode] + ways[node]) % MOD; 
+                }
+            }
+        }
+
+        return ways[n - 1];
+    }
+
+
 
 
 

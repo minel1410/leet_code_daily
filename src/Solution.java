d4119a2e81b01e364c4da71802108c14f4975827
minel1410 <msalihagic@hotmail.com>
2025-03-24 13:45:56 +0100

    Add solution for problem 79

--- src/Solution.java
+++ src/Solution.java
@@ -1646,12 +1646,43 @@ public class Solution {
         
     }
 
+    /*
+     * Given an m x n grid of characters board and a string word, return true if
+     * word exists in the grid.
+     * 
+     * The word can be constructed from letters of sequentially adjacent cells,
+     * where adjacent cells are horizontally or vertically neighboring. The same
+     * letter cell may not be used more than once.
+     */
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length, cols = board[0].length;
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length())
+            return true; 
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index))
+            return false;
 
+        char temp = board[i][j];
+        board[i][j] = '#'; 
 
-   
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                dfs(board, word, i - 1, j, index + 1) ||
+                dfs(board, word, i, j + 1, index + 1) ||
+                dfs(board, word, i, j - 1, index + 1);
 
-
+        board[i][j] = temp; 
+        return found;
+    }
 
 }

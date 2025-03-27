6aec90c0ec1d51985b46e6f2b1c5e0c12efc174a
minel1410 <msalihagic@hotmail.com>
2025-03-27 12:34:39 +0100

    Add solution for problem 2780

--- src/Solution.java
+++ src/Solution.java
@@ -2077,4 +2077,61 @@ public class Solution {
 
         return node; 
     }
+
+
+    /*
+     * An element x of an integer array arr of length m is dominant if more than
+     * half the elements of arr have a value of x.
+     * 
+     * You are given a 0-indexed integer array nums of length n with one dominant
+     * element.
+     * 
+     * You can split nums at an index i into two arrays nums[0, ..., i] and nums[i +
+     * 1, ..., n - 1], but the split is only valid if:
+     * 
+     * 0 <= i < n - 1
+     * nums[0, ..., i], and nums[i + 1, ..., n - 1] have the same dominant element.
+     * Here, nums[i, ..., j] denotes the subarray of nums starting at index i and
+     * ending at index j, both ends being inclusive. Particularly, if j < i then
+     * nums[i, ..., j] denotes an empty subarray.
+     * 
+     * Return the minimum index of a valid split. If no valid split exists, return
+     * -1.
+    
+     */
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
+
+        HashMap<Integer, Integer> frekvencija = new HashMap<>();
+        for (int num : nums) {
+            frekvencija.put(num, frekvencija.getOrDefault(num, 0) + 1);
+        }
+
+        int dominantni = -1, maxFrekvencija = 0;
+        for (Map.Entry<Integer, Integer> entry : frekvencija.entrySet()) {
+            if (entry.getValue() * 2 > n) { 
+                dominantni = entry.getKey();
+                maxFrekvencija = entry.getValue();
+                break;
+            }
+        }
+
+        if (dominantni == -1)
+            return -1; 
+
+        int count_prije = 0, count_poslije = maxFrekvencija;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums.get(i) == dominantni) {
+                count_prije++;
+                count_poslije--;
+            }
+
+            if (count_prije * 2 > (i + 1) && count_poslije * 2 > (n - i - 1)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }

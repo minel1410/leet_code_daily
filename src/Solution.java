4c1c7d386899c3a8b1abade87adf15c5f971c3d8
minel1410 <msalihagic@hotmail.com>
2025-03-31 23:43:37 +0200

    Add solution for problem 2551

--- src/Solution.java
+++ src/Solution.java
@@ -2437,4 +2437,43 @@ public class Solution {
         }
         return partitionSizes;
     }
+
+
+
+    /*
+     * You have k bags. You are given a 0-indexed integer array weights where
+     * weights[i] is the weight of the ith marble. You are also given the integer k.
+     * 
+     * Divide the marbles into the k bags according to the following rules:
+     * 
+     * No bag is empty.
+     * If the ith marble and jth marble are in a bag, then all marbles with an index
+     * between the ith and jth indices should also be in that same bag.
+     * If a bag consists of all the marbles with an index from i to j inclusively,
+     * then the cost of the bag is weights[i] + weights[j].
+     * The score after distributing the marbles is the sum of the costs of all the k
+     * bags.
+     * 
+     * Return the difference between the maximum and minimum scores among marble
+     * distributions.
+     */
+    public long putMarbles(int[] weights, int k) {
+        // We collect and sort the value of all n - 1 pairs.
+        int n = weights.length;
+        int[] pairWeights = new int[n - 1];
+        for (int i = 0; i < n - 1; ++i) {
+            pairWeights[i] = weights[i] + weights[i + 1];
+        }
+        // We will sort only the first (n - 1) elements of the array.
+        Arrays.sort(pairWeights, 0, n - 1);
+
+        // Get the difference between the largest k - 1 values and the
+        // smallest k - 1 values.
+        long answer = 0l;
+        for (int i = 0; i < k - 1; ++i) {
+            answer += pairWeights[n - 2 - i] - pairWeights[i];
+        }
+
+        return answer;
+    }
 }
